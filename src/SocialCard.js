3b149a04cf6e94f30d0d067a4d4c025bf0f72eca
AhmedElaguab <elaguabahmed97@gmail.com>
2019-02-12 23:59:07 +0100

    feat: add the time of the post to the componenet

--- src/SocialCard.js
+++ src/SocialCard.js
@@ -7,45 +7,58 @@ class SocialCard extends Component {
   render() {
     return (
       <div className="social-cards">
-        {this.props.data.map(card => (
-          <div className="social-card" key={card.id_str}>
-            <div className="social-card-user">
-              <div className="social-card-user-avatar">{card.user.name[0]}</div>
-              <div className="social-card-user-name">
-                <p>
-                  {card.user.name}
-                  <a
-                    className="social-card-user-url"
-                    href={card.user.url}
-                    target="blank"
-                  >
-                    <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" />
-                  </a>
-                </p>
-                <p className="social-card-user-screen_name">
-                  @{card.user.screen_name} - {card.user.location}
-                </p>
+        {this.props.data.map(card => {
+          var date = new Intl.DateTimeFormat("en-US", {
+            format: "long"
+          }).format(new Date(card.created_at));
+
+          return (
+            <div className="social-card" key={card.id_str}>
+              <div className="social-card-user">
+                <div className="social-card-user-avatar">
+                  {card.user.name[0]}
+                </div>
+                <div className="social-card-user-name">
+                  <p>
+                    {card.user.name}
+                    <a
+                      className="social-card-user-url"
+                      href={card.user.url}
+                      target="blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" />
+                    </a>
+                  </p>
+                  <p className="social-card-user-screen_name">
+                    @{card.user.screen_name} - {card.user.location}
+                  </p>
+                </div>
+              </div>
+              <div className="social-card-body">
+                <div className="social-card-body-entities">
+                  {card.entities.urls && (
+                    <a
+                      href={card.entities.urls[0].url}
+                      className="social-card-body-entities-url"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {card.entities.urls[0].unwound.title}
+                      <br />
+                      <hr />
+                      <small>{card.entities.urls[0].url}</small>
+                    </a>
+                  )}
+                </div>
+                <p className="social-card-body-p">{card.text}</p>
+              </div>
+              <div className="social-card-footer">
+                <p className="social-card-footer-date">{date}</p>
               </div>
             </div>
-            <div className="social-card-body">
-              <div className="social-card-body-entities">
-                {card.entities.urls && (
-                  <a
-                    href={card.entities.urls[0].url}
-                    className="social-card-body-entities-url"
-                    target="_blank"
-                  >
-                    {card.entities.urls[0].unwound.title}
-                    <br />
-                    <hr />
-                    <small>{card.entities.urls[0].url}</small>
-                  </a>
-                )}
-              </div>
-              <p className="social-card-body-p">{card.text}</p>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     );
   }
